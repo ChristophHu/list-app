d2c25220b92bac3e06cc9867c3f645e35b97631d
christophhu <christoph.huschenhoefer@gmx.de>
2022-07-14 18:25:59 +0200

    add local strategy

--- list-app-backend/src/modules/authentication/guards/local.strategy.ts
+++ list-app-backend/src/modules/authentication/guards/local.strategy.ts
@@ -1,22 +1,20 @@
-// import { Injectable } from "@nestjs/common";
-// import { PassportStrategy } from "@nestjs/passport";
-// import { AuthService } from "../auth.service";
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
 
-// @Injectable()
-// export class LocalStrategy extends PassportStrategy(Strategy) {
-//   constructor(private authService: AuthService) {
-//     super({ usernameField: 'email' });
-//   }
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
 
-// //   async validate(email: string, password: string): Promise<any> { // class is constructed but this method is never called
-// //     const user: UserDto = await this.authService.login({
-// //       email,
-// //       password,
-// //     });
-    
-// //     if (!user) {
-// //       throw new UnauthorizedException();
-// //     }
-// //     return user;
-// //   }
-// }
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService..validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+    return null
+  }
+}
